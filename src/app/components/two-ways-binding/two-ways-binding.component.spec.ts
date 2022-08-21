9ad78e2ce816cfa4d214a09d95e4a7a7656412e7
grazirs <graziribeiro95@gmail.com>
2022-08-21 16:42:31 +0200

    add two-way data binding example

---
+++ src/app/components/two-ways-binding/two-ways-binding.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TwoWaysBindingComponent } from './two-ways-binding.component';
+
+describe('TwoWaysBindingComponent', () => {
+  let component: TwoWaysBindingComponent;
+  let fixture: ComponentFixture<TwoWaysBindingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TwoWaysBindingComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TwoWaysBindingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/two-ways-binding/two-ways-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-ways-binding',
+  templateUrl: './two-ways-binding.component.html',
+  styleUrls: ['./two-ways-binding.component.css']
+})
+export class TwoWaysBindingComponent implements OnInit {
+  name: String = ''
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
