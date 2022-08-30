aeba51ffd85dd6a22a9336814ef840444b5bc803
grazirs <graziribeiro95@gmail.com>
2022-08-30 13:37:09 +0200

    add dynamic route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DirectivesComponent } from "./components/directives/directives.componen
 import { EmitterComponent } from "./components/emitter/emitter.component";
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { IfRenderComponent } from "./components/if-render/if-render.component";
+import { ItemDetailComponent } from "./components/item-detail/item-detail.component";
 import { ListRenderComponent } from "./components/list-render/list-render.component";
 import { PipesComponent } from "./components/pipes/pipes.component";
 import { TwoWaysBindingComponent } from "./components/two-ways-binding/two-ways-binding.component";
@@ -17,6 +18,7 @@ const routes: Routes = [
   {path: 'if-render', component: IfRenderComponent},
   {path: 'pipes', component: PipesComponent},
   {path: 'two-way-binding', component: TwoWaysBindingComponent},
+  {path: 'list/:id', component: ItemDetailComponent},
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ChangeNumberComponent } from './components/change-number/change-number.
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { PipesComponent } from './components/pipes/pipes.component';
 import { TwoWaysBindingComponent } from './components/two-ways-binding/two-ways-binding.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { TwoWaysBindingComponent } from './components/two-ways-binding/two-ways-
     ChangeNumberComponent,
     ListRenderComponent,
     PipesComponent,
-    TwoWaysBindingComponent
+    TwoWaysBindingComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule, FormsModule, AppRoutingModule, HttpClientModule
